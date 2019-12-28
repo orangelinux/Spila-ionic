758f5c41fd93f095c486d4fc813e2518d4375515
orangelinux <verneemix2.vlv@gmail.com>
2019-12-28 19:38:22 +0900

    all setting complete

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -26,23 +26,38 @@ export class Tab3Page {
     console.log(fnc);
     await this.setVal();
     if (fnc == "crack") {
-      const loading = await this.loadingController.create({
-        message: '通信中です..お待ちください...',
-        translucent: true,
-        cssClass: 'custom-class custom-loading'
-      });
-      loading.present();
-      console.log("crack");
-        this.http.get('https://spmoveapi.herokuapp.com/setting?TF=' +tf+ '&change=1&user=' + this.user + '&D1=' + this.D1 + '&D2=' + this.D2, {}, {})
-          .then(data => {
-            console.log(data.data); 
-            loading.dismiss();
-
-  })
-  .catch(error => {
-    console.log(error.status);
-  });
+      this.SetSend(tf, 1);
+    } else if (fnc == "business") {
+      this.SetSend(tf, 0);
+    } else if (fnc == "otherspam") {
+      this.SetSend(tf, 2);
+    }else if (fnc == "notify") {
+      this.SetSend(tf, 3);
+    } else if (fnc == "analysis") {
+      this.SetSend(tf, 4);
+    }else if (fnc == "ahead") {
+      this.SetSend(tf, 5);
+    } else {
+      console.log("FNC ERR");
+      console.log(fnc);
     }
+  }
+  async SetSend(tf,change) {
+    const loading = await this.loadingController.create({
+      message: '通信中です..お待ちください...',
+      translucent: true,
+      cssClass: 'custom-class custom-loading'
+    });
+    loading.present();
+    console.log("otherspam");
+      this.http.get('https://spmoveapi.herokuapp.com/setting?TF=' +tf+ '&change=' + change + '&user=' + this.user + '&D1=' + this.D1 + '&D2=' + this.D2, {}, {})
+        .then(data => {
+          console.log(data.data); 
+          loading.dismiss();
+})
+.catch(error => {
+  console.log(error.status);
+});
   }
   async setVal() {
     console.log("open Setval!");
