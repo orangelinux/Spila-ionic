c7e3547f8fa524347866e35ff15cb74d899e2062
orangelinux <verneemix2.vlv@gmail.com>
2020-01-10 23:15:44 +0900

    this is Working

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,6 +9,10 @@ import { Storage } from '@ionic/storage';
 export class Tab2Page {
   notify: any;
   noupdate: any;
+  n1: any;
+  n2: any;
+  n3: any;
+  n4: any;
   notifyarray: any;
   resnotify: any;
   constructor(private storage: Storage, private http: HTTP) { }
@@ -18,7 +22,7 @@ export class Tab2Page {
   }
   
   async Newnotifycheck() {
-    this.http.get('https://spmoveapi.herokuapp.com/getnotify', {}, {})
+    await this.http.get('https://spmoveapi.herokuapp.com/getnotify', {}, {})
       .then(data => {
         //ここからjsonの処理
         console.log(data.status);
@@ -56,11 +60,16 @@ export class Tab2Page {
   async setstorage(ARRAY) {
     console.log("boot setstorage");
     try {
-      this.storage.get('notify').then((val) => {
-        this.notifyarray = val;
+      await this.storage.get('notify').then((val) => {
+        if (val) {
+          this.notifyarray = val;
+        } else {
+          this.notifyarray = [];
+        }
         this.notifyarray.push(ARRAY);
+        this.storage.set('notify', this.notifyarray);
       });
-      this.viewnotify();
+      await this.viewnotify();
     } catch {
       console.log("ERR");
     }
@@ -71,8 +80,21 @@ export class Tab2Page {
       if (val) {
         this.resnotify = val;
         console.log(this.resnotify);
+        if (this.resnotify[0]) {
+          this.n1 = this.resnotify[0];
+        }
+        if (this.resnotify[1]) {
+          this.n2 = this.resnotify[1];
+        }
+        if (this.resnotify[2]) {
+          this.n3 = this.resnotify[2];
+        }
+        if (this.resnotify[3]) {
+          this.n4 = this.resnotify[3];
+        }
       } else {
         console.log("val empty");
+        console.log(this.resnotify);
       }
     });
  }
