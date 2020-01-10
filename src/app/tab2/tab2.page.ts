f31667897ea9ce9786a7baea201e856bb1db5adf
orangelinux <verneemix2.vlv@gmail.com>
2020-01-10 15:23:32 +0900

    Fix

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,48 +9,56 @@ import { Storage } from '@ionic/storage';
 export class Tab2Page {
   notify: any;
   notifyarray: any;
-  constructor(private storage: Storage, private http: HTTP){}
+  resnotify: any;
+  constructor(private storage: Storage, private http: HTTP) { }
   ngOnInit() {
     console.log("NGONINIT");
     this.Newnotifycheck();
-}
+  }
+  
   Newnotifycheck() {
     this.http.get('https://spmoveapi.herokuapp.com/getnotify', {}, {})
-    .then(data => {
+      .then(data => {
   
-      console.log(data.status);
-      console.log(data.data); // data received by server
-      var replaced = data.data.replace(/'/g, '"');
-      console.log(replaced);
-      var res = JSON.parse(replaced);
-      var latest = res["latest"];
-      try {
+        console.log(data.status);
+        console.log(data.data); // data received by server
+        var replaced = data.data.replace(/'/g, '"');
+        console.log(replaced);
+        var res = JSON.parse(replaced);
+        var latest = res["latest"];
+        try {
+          this.storage.get('notify').then((val) => {
+            console.log('==', val);
+            const notifyarray = [];
+            if (!val) {
+              notifyarray.push(latest);
+              this.notifyarray = notifyarray;
+              console.log(this.notifyarray);
+            } else {
+              this.notify = val;
+              notifyarray.push(this.notify);
+              notifyarray.push(latest);
+            }
+            notifyarray.push(this.notify);
+          });
+        } catch {
+          this.notifyarray = this.notify;
+        }
+        this.storage.set('notify', this.notifyarray);
 
         this.storage.get('notify').then((val) => {
           console.log('==', val);
-
-          if (!this.notify) {
-            this.notify = val;
-            const notifyarray = [];
-            notifyarray.push(latest);
-            notifyarray.push(this.notify);
-            this.notifyarray = notifyarray;
-            console.log(this.notifyarray);
-          }
-        });
-
-        } catch {
-        this.notifyarray = this.notify;
-        }
-      this.storage.set('notify',this.notifyarray);
+          this.resnotify = val;
+          console.log(this.resnotify);
+        })
+      })
+      .catch(error => {
+        console.log(error);
+        console.log(error.status);
+        console.log(error.error); // error message as string
+        console.log(error.headers);
   
-    })
-    .catch(error => {
-      console.log(error);
-      console.log(error.status);
-      console.log(error.error); // error message as string
-      console.log(error.headers);
-  
-    });
+      });
+      
   }
 }
